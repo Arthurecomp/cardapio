f5646815123c006ddd7fa0bd475a814e1323a979
Arthur Marcelino <arthurecomp22@gmail.com>
2025-04-03 19:03:57 -0300

    initial project

---
+++ src/main/java/com/cardapio/CardarpioApplication.java
@@ -0,0 +1,13 @@
+package com.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardarpioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardarpioApplication.class, args);
+	}
+
+}
