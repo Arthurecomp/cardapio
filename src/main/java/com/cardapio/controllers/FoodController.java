ed7394e1c9df5f46d9dc46191b0ccc1db0e7e894
Arthur Marcelino <arthurecomp22@gmail.com>
2025-04-03 19:03:30 -0300

    create food controller

---
+++ src/main/java/com/cardapio/controllers/FoodController.java
@@ -0,0 +1,31 @@
+package com.cardapio.controllers;
+
+import com.cardapio.domain.Food;
+import com.cardapio.dtos.FoodRequestDTO;
+import com.cardapio.dtos.FoodResponseDTO;
+import com.cardapio.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+    
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+    }
+}
